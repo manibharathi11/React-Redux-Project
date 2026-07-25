08571b70066367bcfbd409fb36d810fd82ef55cd
manibharathi11 <manibharathiinreallife@gmail.com>
2026-07-25 20:33:51 +0530

    + redux, router and few initial setup

--- React-Redux-Sample-project/src/App.jsx
+++ React-Redux-Sample-project/src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>hello, this is the sample react redux project</h1>
+    </>
+  );
 }
 
 export default App;
